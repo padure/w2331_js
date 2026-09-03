e99c1d4eece148ca4833771eba41eca42d56551d
padure <paduregheorghe7@gmail.com>
2026-09-03 09:30:30 +0300

    Laborator 2

---
+++ Grupa_2/laborator_1/app.js
@@ -0,0 +1,18 @@
+console.log(nume);
+var nume    = "Doe";
+var prenume = "John";
+let varsta;
+const PORT = 8080;
+
+// nume = "Alin";
+varsta = 25;
+// PORT = 43;
+// nume = 2026;
+
+console.log(varsta);
+console.log(nume);
+console.log(PORT);
+console.log(nume + ' ' + prenume + ' ' + varsta);
+console.log(`${nume} ${prenume} ${varsta}`);
+
+document.write(nume);
---
+++ README.md
@@ -0,0 +1,2 @@
+# w2431_js
+Programarea client-side a site-urilor Web
